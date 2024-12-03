cb90c6ebd582d3ec6abb5ea6271e02c314ea6316
Sergey Bondarenko <sergeybondarenko@macbookpro.local>
2024-12-03 18:53:49 +0300

    feat: API for adding counter/gauge metrics

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,88 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+)
+
+type MemStorageFuncs interface {
+	getCounter()
+	getGauge()
+	setCounter()
+	setGauge()
+}
+
+type MemStorage struct {
+	counter map[string]int64
+	gauge   map[string]float64
+}
+
+func (memStorage MemStorage) getCounter(name string) int64 {
+	return memStorage.counter[name]
+}
+
+func (memStorage MemStorage) getGauge(name string) float64 {
+	return memStorage.gauge[name]
+}
+
+func (memStorage MemStorage) setCounter(name string, value int64) {
+	memStorage.counter[name] += value
+}
+
+func (memStorage MemStorage) setGauge(name string, value float64) {
+	memStorage.gauge[name] = value
+}
+
+func updateMetric(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		metricType := req.PathValue("metricType")
+		metricName := req.PathValue("metricName")
+		metricValue := req.PathValue("metricValue")
+
+		if metricType == "counter" || metricType == "gauge" {
+			if metricName == "" {
+				res.WriteHeader(http.StatusNotFound)
+				return
+			} else {
+
+				if metricType == "counter" {
+					_, err := strconv.Atoi(metricValue)
+					if err == nil {
+						//
+					} else {
+						res.WriteHeader(http.StatusBadRequest)
+						return
+					}
+				} else {
+					_, err := strconv.ParseFloat(metricValue, 64)
+					if err == nil {
+						//
+					} else {
+						res.WriteHeader(http.StatusBadRequest)
+						return
+					}
+				}
+
+				res.Header().Set("content-type", "text/plain; charset=utf-8")
+				res.WriteHeader(http.StatusOK)
+			}
+		} else {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		return
+	} else {
+		http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/update/{metricType}/{metricName}/{metricValue}`, updateMetric)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sb-truefalse/observator
+
+go 1.23.1
